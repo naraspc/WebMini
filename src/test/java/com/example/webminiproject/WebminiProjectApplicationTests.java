015b824f20e89cb926d5db9b54756471f52da242
naraspc <wodnrtkfka@naver.com>
2023-11-29 00:48:19 +0900

    add project

---
+++ src/test/java/com/example/webminiproject/WebminiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebminiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
